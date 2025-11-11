17ff735757a552f25028519294b826300ff9c7c2
Rahgir Arefin Rafi <112659971+rahgirrafi@users.noreply.github.com>
2025-11-11 16:47:55 +0600

    fixed code

--- pc_switch.ino
+++ pc_switch.ino
@@ -1,45 +1,36 @@
 #define BLYNK_TEMPLATE_ID "TMPL6SEwTof73"
-#define BLYNK_TEMPLATE_NAME "ESP32"
+#define BLYNK_TEMPLATE_NAME "ESP8266"
 #define BLYNK_AUTH_TOKEN "xvjiSfYanFxZHRacHdLfA9d0ej_nSP5o"
 
-#include <WiFi.h>
+#include <ESP8266WiFi.h>
 #include <WiFiClient.h>
-#include <BlynkSimpleEsp32.h>
+#include <BlynkSimpleEsp8266.h>
 
 
-
-// Your Blynk authentication token
 char auth[] = BLYNK_AUTH_TOKEN;
 
 // Your Wi-Fi credentials
 char ssid[] = "OptimusPrime";
 char pass[] = "05061335";
 
-// Define GPIO pin (ESP32's onboard LED is typically on GPIO2)
 #define Switch_PIN 4
 
-// Blynk virtual pin handler
 BLYNK_WRITE(V0) { // Virtual pin V0
   int pinValue = param.asInt(); // Get value from virtual pin
   digitalWrite(Switch_PIN, pinValue); // Set GPIO2 state
 }
 
 void setup() {
-  // Initialize serial monitor
   Serial.begin(115200);
-  // Configure GPIO2 as output
   pinMode(Switch_PIN, OUTPUT);
   digitalWrite(Switch_PIN, HIGH);
-  
-  // Connect to Wi-Fi and Blynk
   Blynk.begin(auth, ssid, pass);
-  
-  // Optional: Wait for connection
-  while (Blynk.connect() == false) {
-    // Wait until connected
+
+  if (Blynk.connected()) {
+    Serial.println("Connected to Blynk!");
+  } else {
+    Serial.println("Blynk connection failed, but continuing...");
   }
-  
-  Serial.println("Connected to Blynk!");
 }
 
 void loop() {
